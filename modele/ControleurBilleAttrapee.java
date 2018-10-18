701c9fb87b1ec2ff720d147f3af42eee9372925d
NwotYknuf <jlafille@ensc.fr>
2018-10-18 10:24:57 +0200

    Creation Controleur bille attrapee

---
+++ modele/ControleurBilleAttrapee.java
@@ -0,0 +1,25 @@
+package angry_balls.modele;
+
+public class ControleurBilleAttrapee extends ControleurBille {
+
+    public ControleurBilleAttrapee(BillePrehensible bille, ControleurBille precedant, ControleurBille suivant){
+        super(bille, precedant, suivant);
+    }
+
+    public void actionDetectee() {
+        // TODO
+    }
+
+    public void traite() {
+        // TODO
+    }
+
+    public void etatSuivant() {
+        // TODO
+    }
+
+    public void etatPrecedant() {
+        // TODO
+    }
+
+}

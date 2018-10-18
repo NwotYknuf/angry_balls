80316e0fb889d2e0073477c06747fb5dd5a58a84
NwotYknuf <jlafille@ensc.fr>
2018-10-18 09:15:27 +0200

    Creation de IComportementAcceleration

---
+++ modele/IComportementCollision.java
@@ -0,0 +1,7 @@
+package angry_balls.modele;
+
+public interface IComportementCollision{
+
+    public void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur,double hauteur);
+
+}

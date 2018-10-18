f62d49e15bbb8b5ba03507305cec4a6d85818bd7
NwotYknuf <jlafille@ensc.fr>
2018-10-18 09:23:02 +0200

    ComportementCollision devient une classe

--- modele/ComportementCollision.java
+++ modele/ComportementCollision.java
@@ -1,6 +1,6 @@
 package angry_balls.modele;
 
-public interface IComportementCollision{
+public interface ComportementCollision{
 
     public void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur,double hauteur);
 

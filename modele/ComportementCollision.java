290a637b73a83685558f067187e0236abdcd2890
NwotYknuf <jlafille@ensc.fr>
2018-10-18 09:48:31 +0200

    constructeur et getter

--- modele/ComportementCollision.java
+++ modele/ComportementCollision.java
@@ -2,6 +2,16 @@ package angry_balls.modele;
 
 public abstract class ComportementCollision{
 
+    private Bille billeCourrante;
+
+    ComportementCollision(Bille billeCourante){
+        this.billeCourrante = billeCourrante;
+    }
+
+    public Bille getBilleCourrante(){
+        return billeCourrante;
+    }
+
     public abstract void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur,double hauteur);
 
 }

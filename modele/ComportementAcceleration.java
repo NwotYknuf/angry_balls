5bcc7ef7e3df94483adaee34058fd71502831c49
NwotYknuf <jlafille@ensc.fr>
2018-10-18 09:47:26 +0200

    Constructeur et getter

--- modele/ComportementAcceleration.java
+++ modele/ComportementAcceleration.java
@@ -4,6 +4,16 @@ import java.util.Vector;
 
 public abstract class ComportementAcceleration{
 
+    private Bille billeCourrante;
+
+    ComportementAcceleration(Bille billeCourante){
+        this.billeCourrante = billeCourrante;
+    }
+
+    public Bille getBilleCourrante(){
+        return billeCourrante;
+    }
+
     public abstract void gestionAcceleration(Vector<Bille> billes);
 
 }

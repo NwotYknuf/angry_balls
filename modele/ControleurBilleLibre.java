ec5f9de1bd9d91456e03667b1cc5d75865319a22
NwotYknuf <jlafille@ensc.fr>
2018-10-18 10:25:12 +0200

    creation Controlleur Bille libre

---
+++ modele/ControleurBilleLibre.java
@@ -0,0 +1,25 @@
+package angry_balls.modele;
+
+public class ControleurBilleLibre extends ControleurBille {
+
+    public ControleurBilleLibre(BillePrehensible bille, ControleurBille precedant, ControleurBille suivant){
+        super(bille, precedant, suivant);
+    }
+
+    public void actionDetectee() {
+        // TODO
+    }
+
+    public void traite() {
+        // TODO
+    }
+
+    public void etatSuivant() {
+        // TODO
+    }
+
+    public void etatPrecedant() {
+        // TODO
+    }
+
+}

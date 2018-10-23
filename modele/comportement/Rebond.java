2f3d946305fac29233496468eb6532859ccdc6fc
NwotYknuf <jlafille@ensc.fr>
2018-10-23 15:11:22 +0200

    Rebond

---
+++ modele/comportement/Rebond.java
@@ -0,0 +1,23 @@
+package angry_balls.modele.comportement;
+
+import angry_balls.modele.*;
+import angry_balls.mesmaths.cinematique.Collisions;
+
+public class Rebond extends ComportementCollision{
+
+    public Rebond(Bille bille){
+        super(bille);
+    }
+
+    public void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur, double hauteur){
+
+        Bille billeCourante = this.getBilleCourante();
+
+        Collisions.collisionBilleContourAvecRebond(
+            billeCourante.getPosition(), 
+            billeCourante.getRayon(), 
+            billeCourante.getVitesse(),
+            abscisseCoinHautGauche, ordonneeCoinHautGauche, largeur, hauteur);
+    }
+
+}

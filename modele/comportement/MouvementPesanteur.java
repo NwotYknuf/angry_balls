d12cb2f69f949d48fb77b79ded2499d0a1cc6eea
NwotYknuf <jlafille@ensc.fr>
2018-10-23 14:56:58 +0200

    MouvementPesanteur

---
+++ modele/comportement/MouvementPesanteur.java
@@ -0,0 +1,22 @@
+package angry_balls.modele.comportement;
+
+import java.util.Vector;
+
+import angry_balls.modele.*;
+import angry_balls.mesmaths.geometrie.base.Vecteur;
+
+public class MouvementPesanteur extends ComportementAcceleration{
+
+    private Vecteur pesanteur;
+
+    public MouvementPesanteur(Bille bille, Vecteur pesanteur){
+        super(bille);
+        this.pesanteur = pesanteur;
+    }
+
+    public void gestionAcceleration(Vector<Bille> billes){
+        this.getBilleCourante().getAcceleration().ajoute(
+            this.pesanteur);
+    }
+
+}

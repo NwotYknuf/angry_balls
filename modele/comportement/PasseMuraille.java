c1c1ec661b98646a5e9ac25133ad8313d0e7aba0
NwotYknuf <jlafille@ensc.fr>
2018-10-23 15:11:16 +0200

    PasseMuraille

---
+++ modele/comportement/PasseMuraille.java
@@ -0,0 +1,22 @@
+package angry_balls.modele.comportement;
+
+import angry_balls.modele.*;
+import angry_balls.mesmaths.cinematique.Collisions;
+
+public class PasseMuraille extends ComportementCollision{
+
+    public PasseMuraille(Bille bille){
+        super(bille);
+    }
+
+    public void collisionContour(double abscisseCoinHautGauche, double ordonneeCoinHautGauche, double largeur, double hauteur){
+
+        Bille billeCourante = this.getBilleCourante();
+
+        Collisions.collisionBilleContourPasseMuraille(
+            billeCourante.getPosition(), 
+            abscisseCoinHautGauche,ordonneeCoinHautGauche, largeur, hauteur);
+
+    }
+
+}

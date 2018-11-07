9e81904a202c7d12cce392347634d571af479a1a
NwotYknuf <jlafille@ensc.fr>
2018-11-07 14:41:48 +0100

    ajouté des trucs dans app

--- controleur/App.java
+++ controleur/App.java
@@ -1,6 +1,9 @@
 package angry_balls.controleur;
 
+import javax.xml.bind.Marshaller.Listener;
+
 import angry_balls.AnimationBilles;
+import angry_balls.outilsvues.EcouteurTerminaison;
 import angry_balls.vues.CadreAngryBalls;
 
 public class App{
@@ -9,13 +12,32 @@ public class App{
     private AnimationBilles animationBilles;
     private CadreAngryBalls cadreAngryBalls;
 
+    private Listener l;
+
+    public App(AnimationBilles animationBilles, CadreAngryBalls cadreAngryBalls){
+
+        this.animationBilles = animationBilles;
+        this.cadreAngryBalls = cadreAngryBalls;
+        
+        ControleurBilleLibre bl = new ControleurBilleLibre(this);
+        ControleurBilleAttrapee ba = new ControleurBilleAttrapee(this);
+
+        bl.setSuivant(ba);
+        ba.setPrecedent(bl);
+
+        controleurCourant = bl;
+
+        cadreAngryBalls.lancerBilles.addActionListener(new EcouteurBoutonLancer(animationBilles));
+        cadreAngryBalls.arreterBilles.addActionListener(new EcouteurBoutonArreter(animationBilles));
+        cadreAngryBalls.addWindowListener(new EcouteurTerminaison(cadreAngryBalls));
 
 
-
+    }
 
     public void setControleurCourant(ControleurState controleur){
         controleurCourant = controleur;
     }
+
 
 
 
